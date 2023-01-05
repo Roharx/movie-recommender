f40ac9318ba8844d9327d86e55268e4eb77ed89b
DoinavD <114615981+DoinavD@users.noreply.github.com>
2023-01-05 12:49:13 +0100

    add progress bar and volume bar

--- src/gui/controller/MovieController.java
+++ src/gui/controller/MovieController.java
@@ -1,12 +1,11 @@
 package gui.controller;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
-import javafx.scene.control.ProgressBar;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
@@ -14,8 +13,12 @@ import javafx.scene.media.MediaView;
 import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MovieController implements Initializable {
+    @FXML
+    private Slider volumeSlider;
     @FXML
     private ListView moviesView;
     @FXML
@@ -33,6 +36,10 @@ public class MovieController implements Initializable {
     private File file;
     private Media media;
     private MediaPlayer mediaPlayer;
+    private Timer timer;
+    private TimerTask task;
+    private boolean running;
+
 
 
     @Override
@@ -45,10 +52,41 @@ public class MovieController implements Initializable {
     }
 
     public void playMedia(ActionEvent actionEvent) {
+        beginTimer();
         mediaPlayer.play();
+        changeVolume();
     }
 
     public void pauseMedia(ActionEvent actionEvent) {
         mediaPlayer.pause();
     }
+    public void beginTimer() {
+        timer = new Timer();
+        task = new TimerTask() {
+            public void run() {
+                running = true;
+                double current = mediaPlayer.getCurrentTime().toSeconds();
+                double end = media.getDuration().toSeconds();
+
+                progressBar.setProgress(current/end);
+                if(current/end == 1){
+                    cancelTimer();
+                }
+            }
+        };
+        timer.scheduleAtFixedRate(task, 0, 1000);
+    }
+    public void cancelTimer(){
+        running = false;
+        timer.cancel();
+    }
+    public void changeVolume() {
+        volumeSlider.valueProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                mediaPlayer.setVolume(volumeSlider.getValue() * 0.01);}
+        });
+    }
+
+
 }
