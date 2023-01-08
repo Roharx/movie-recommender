d84b3cba83bf85aad1b8875c9921b18fecb8f2c3
DoinavD <114615981+DoinavD@users.noreply.github.com>
2023-01-08 10:27:25 +0100

    Categoru / CategoryDAO

--- src/dal/CategoryDAO.java
+++ src/dal/CategoryDAO.java
@@ -1,6 +1,76 @@
 package dal;
 
+import be.Category;
+import com.microsoft.sqlserver.jdbc.SQLServerException;
+import dal.database.DatabaseConnector;
 import dal.interfaces.ICategoryDAO;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
 public class CategoryDAO implements ICategoryDAO {
+
+    private PreparedStatement preparedStatement;
+    private DatabaseConnector databaseConnector;
+    public CategoryDAO() { databaseConnector = new DatabaseConnector();}
+
+    @Override
+     public List<Category> getAllCategories() throws SQLException{
+        List<Category> categories = new ArrayList<>();
+
+        String sql = "SELECT * FROM Category";
+        preparedStatement = databaseConnector.createConnection().prepareStatement(sql);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        while (resultSet.next()) {
+            categories.add(new Category(
+                    resultSet.getInt("id"),
+                    resultSet.getString("categoryname")
+            ));
+        }
+        return categories;
+    }
+
+    @Override
+    public Category getCategoryByID(int id) throws SQLException {
+        Category result = null;
+
+        String sql = "SELECT * FROM Category WHERE id = ?";
+
+        preparedStatement = databaseConnector.createConnection().prepareStatement(sql);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        if(resultSet.next())
+            result = new Category(
+                    resultSet.getInt("id"),
+                    resultSet.getString("categoryname")
+            );
+        return result;
+    }
+
+    @Override
+     public void createCategory( Category category) throws SQLException {
+        String sql = "INSERT INTO Category( id, categoryname ) VALUES ( ?,?)";
+
+        preparedStatement = databaseConnector.createConnection().prepareStatement(sql);
+
+        preparedStatement.setInt(1, category.getId());
+        preparedStatement.setString(2,category.getCategoryname());
+
+        preparedStatement.executeQuery();
+    }
+
+
+    @Override
+    public void deleteCategory(int id) throws SQLException {
+        String sql = "DELETE FROM Category WHERE id= ?";
+        Connection conn = databaseConnector.createConnection();
+
+        PreparedStatement psrmt = conn.prepareStatement(sql);
+
+        psrmt.setInt(1,id);
+        psrmt.executeUpdate();
+    }
 }
+
+
--- src/dal/interfaces/ICategoryDAO.java
+++ src/dal/interfaces/ICategoryDAO.java
@@ -1,4 +1,15 @@
 package dal.interfaces;
 
+import be.Category;
+import com.microsoft.sqlserver.jdbc.SQLServerException;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public interface ICategoryDAO {
+    List<Category> getAllCategories() throws SQLException;
+    Category getCategoryByID(int id) throws SQLException;
+    void createCategory(Category category) throws SQLException;
+    void deleteCategory(int id) throws SQLException;
+
 }
