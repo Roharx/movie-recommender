56ae34c36d6a37765ac0cef424c4d332c3033245
LeeVoi <Abual3a7ed.6@gmail.com>
2023-01-09 11:07:23 +0100

    Finished working on CatMovieDAO and CatMovieManager

--- src/bll/interfaces/ICatMovieManager.java
+++ src/bll/interfaces/ICatMovieManager.java
@@ -1,4 +1,10 @@
 package bll.interfaces;
 
+import java.sql.SQLException;
+import java.util.List;
+
 public interface ICatMovieManager {
+    List<Integer> getMovieIDsByCategoryForID(int catID) throws SQLException;
+
+    List<Integer> getCategoriesForMovieID(int movieID) throws SQLException;
 }
--- src/dal/interfaces/ICatMovieDAO.java
+++ src/dal/interfaces/ICatMovieDAO.java
@@ -1,4 +1,13 @@
 package dal.interfaces;
 
+import be.Category;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public interface ICatMovieDAO {
+
+    List<Integer> getMovieIDsByCategoryForID(int catID) throws SQLException;
+
+    List<Integer> getCategoriesForMovieID(int movieID) throws SQLException;
 }
